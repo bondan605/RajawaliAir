774e6f3324876791740a77c41db5e60163e5cd75
Ahmad Bondan Gunawan <bondangunawan77@gmail.com>
2024-01-12 19:38:25 +0700

    booking page layout and item card

--- app/src/main/java/com/rajawali/app/presentation/MainActivity.kt
+++ app/src/main/java/com/rajawali/app/presentation/MainActivity.kt
@@ -2,15 +2,14 @@ package com.rajawali.app.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.rajawali.app.databinding.ActivityMainBinding
+import com.rajawali.app.databinding.FragmentBookingPageBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: FragmentBookingPageBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = FragmentBookingPageBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
     }
-
 }
