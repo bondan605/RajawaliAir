cd539b3eee37dd21c573e953fd28968e35a1d7b9
Ahmad Bondan Gunawan <bondangunawan77@gmail.com>
2024-01-17 09:15:56 +0700

    fragment selected flight and departure schedule item

--- app/src/main/java/com/rajawali/app/presentation/MainActivity.kt
+++ app/src/main/java/com/rajawali/app/presentation/MainActivity.kt
@@ -2,14 +2,14 @@ package com.rajawali.app.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.rajawali.app.databinding.FragmentBookingPageBinding
+import com.rajawali.app.databinding.FragmentSelectedTicketBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: FragmentBookingPageBinding
+    private lateinit var binding: FragmentSelectedTicketBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = FragmentBookingPageBinding.inflate(layoutInflater)
+        binding = FragmentSelectedTicketBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
 }
